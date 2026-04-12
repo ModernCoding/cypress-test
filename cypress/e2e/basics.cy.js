7afa0bbb9b7265d915f41d6e1fe95c519f4f7f4a
ccc.444 <contact@coding-crafting-consulting.com>
2026-04-12 17:23:35 +0700

    basics

--- cypress/e2e/basics.cy.js
+++ cypress/e2e/basics.cy.js
@@ -3,7 +3,11 @@
 describe ('visit website', () => {
   it ('Visit the website', () => {
     // cy.visit ('https://www.saucedemo.com/v1/')
-    cy.visit ('https://www.saucedemo.com/')
-    cy.url().should('include', 'saucedemo')
+    // cy.visit ('https://www.saucedemo.com/')
+    // cy.url().should('include', 'saucedemo')
+    cy
+      .visit ('https://parabank.parasoft.com/parabank/index.htm')
+      .get('.logo')
+      .should('be.visible')
   })
 })

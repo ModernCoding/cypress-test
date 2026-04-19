e6b4c2e2e33449c462ceaf4fbcc0f8efa40f83d6
ccc.444 <contact@coding-crafting-consulting.com>
2026-04-19 17:24:35 +0700

    beforeEach

--- cypress/e2e/basics.cy.js
+++ cypress/e2e/basics.cy.js
@@ -1,11 +1,16 @@
 /// <reference types="cypress" />
 
 describe ('visit website', () => {
-  it ('Visit the website', () => {
-    // cy.visit ('https://www.saucedemo.com/v1/')
-    // cy.visit ('https://www.saucedemo.com/')
-    // cy.url().should('include', 'saucedemo')
+  // cy.visit ('https://www.saucedemo.com/v1/')
+  // cy.visit ('https://www.saucedemo.com/')
+  // cy.url().should('include', 'saucedemo')
+  beforeEach ('Visit the webpage', () => {
     cy.visit ('https://parabank.parasoft.com/parabank/index.htm')
+  })
+  afterEach ('Clean up after test', () => {
+    cy.log ('Test completed')
+  })
+  it ('Visit the website', () => {
     cy.get ('.logo').should ('be.visible')
     cy.get ('.caption').should ('have.text', 'Experience the difference')
   })
